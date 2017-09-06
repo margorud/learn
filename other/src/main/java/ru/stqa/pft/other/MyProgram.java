91e64d4df6bf9cbe41dbca69187808ed1cd2a49c
margorud <margorud@gmail.com>
2017-09-06 16:36:17 +0700

    change classes

--- other/src/main/java/ru/stqa/pft/other/MyProgram.java
+++ other/src/main/java/ru/stqa/pft/other/MyProgram.java
@@ -6,23 +6,22 @@ public class MyProgram {
         hello("user");
         hello("Рита");
 
-        double len = 6;
-        System.out.println("Площадь квадрата со стороной " +  len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " +  s.l + " = " + area(s));
 
-        double a = 2;
-        double b = 4;
-        System.out.println("Площадь прямоугольника со сторонами " +  a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(10, 2);
+        System.out.println("Площадь прямоугольника со сторонами " +  r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double l){
-        return l*l;
+    public static double area (Square s){
+        return s.l*s.l;
     }
 
-    public static double area (double a, double b){
-        return a*b;
+    public static double area (Rectangle r){
+        return r.a*r.b;
     }
 }
---
+++ other/src/main/java/ru/stqa/pft/other/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.other;
+
+public class Square {
+    public double l;
+
+    public Square(double len){
+        this.l = len;
+    }
+}
