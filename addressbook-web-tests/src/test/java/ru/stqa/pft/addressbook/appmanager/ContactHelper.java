2f4e60db952bd93100d7f8ffd977184c5a666030
margorud <margorud@gmail.com>
2017-11-02 17:48:45 +0700

    add testContactModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -36,4 +36,12 @@ public class ContactHelper extends HelperBase {
     public void acceptDeleteContacts() {
         acceptAlert();
     }
+
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
 }
