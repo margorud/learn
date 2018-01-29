a3ca08a2af6b859624575c402a70df5375868af6
margorud <margorud@gmail.com>
2018-01-29 18:29:38 +0700

    добавлено сравнение групп при модификации группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -20,13 +21,17 @@ public class GroupModificationTests extends TestBase{
 
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("qwe1", "qwe2", "qwe3"));
+
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"qwe1", "qwe2", "qwe3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
 
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
-        //Assert.assertEquals(before, after);
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after)); //преобразуем список в неупорядоченное множество
     }
 }
