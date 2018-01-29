1447109478e12d7f462e366ccdb7e2e7f1121316
margorud <margorud@gmail.com>
2018-01-29 14:43:54 +0700

    реализован выбор группы по номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -16,7 +16,7 @@ public class GroupModificationTests extends TestBase{
 
         int before = app.getGroupHelper().getGroupCount();
 
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("qwe1", "qwe2", "qwe3"));
         app.getGroupHelper().submitGroupModification();
